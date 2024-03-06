f4969024da2f7794c74f3cd8709bb240621fd785
narekcpp <94910814+narekcpp@users.noreply.github.com>
2024-03-06 19:59:49 +0400

    first commit

---
+++ gusse_the_number.py
@@ -0,0 +1,28 @@
+from random import randint
+def generate_number():
+    randome_Number=randint(0,10)
+    return randome_Number
+
+def main():
+    the_Number=generate_number()
+    i=1
+    while True:
+        print('Gusse the number')
+        
+        number=input()
+        
+        try:
+            number= int(number)
+        except:
+            print("wrong number")  
+            continue    
+        if number==the_Number:
+            print('cungres  '+str(i))
+            break
+        elif number > the_Number:
+            print('go lower')
+        elif number < the_Number:
+            print('go higher')
+        print("========================")
+        i+=1
+main() 
